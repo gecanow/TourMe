edd8c645d8c05e10d40ad8e9625b74c20cf60ced
Elizabeth Petrov <elizabeth.petrov@gmail.com>
2017-06-07 11:11:07 -0700

    start chat

--- TourMe/MainScreenViewController.swift
+++ TourMe/MainScreenViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainScreenViewController: UINavigationController {
+class MainScreenViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
